55cc08c2484782d95d96bc4bad48596b388ea5bb
Will Dampier <judowill@gmail.com>
2010-06-16 17:13:09 -0400

    fixed indentation

--- PubmedUtils.py
+++ PubmedUtils.py
@@ -6,35 +6,39 @@ import re
 
 
 def GetXML(ID_LIST, db = 'pubmed'):
-	POST_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/epost.fcgi?db=%s' % db
-	RET_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=%s&query_key=1&mode=xml' % db
 
-	pmid_list = ','.join(map(lambda x: str(x), ID_LIST))
-	post_req_url = POST_URL + '&id=' + pmid_list
+    valid_db = set(['pubmed', 'pmc'])
+    assert db in valid_db
 
-	moz_emu = PyMozilla.MozillaEmulator(cacher = None)
-	post_res = moz_emu.download(post_req_url, trycount = 3)
+    POST_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/epost.fcgi?db=%s' % db
+    RET_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=%s&query_key=1&mode=xml' % db
 
-	web_env = re.findall('<WebEnv>(.*?)</WebEnv>', post_res)[0]
+    pmid_list = ','.join(map(lambda x: str(x), ID_LIST))
+    post_req_url = POST_URL + '&id=' + pmid_list
 
-	req_url = RET_URL + '&WebENV=' + web_env
+    moz_emu = PyMozilla.MozillaEmulator(cacher = None)
+    post_res = moz_emu.download(post_req_url, trycount = 3)
 
-	xml_data = moz_emu.download(req_url, trycount = 3)
-	return xml_data
+    web_env = re.findall('<WebEnv>(.*?)</WebEnv>', post_res)[0]
+
+    req_url = RET_URL + '&WebENV=' + web_env
+
+    xml_data = moz_emu.download(req_url, trycount = 3)
+    return xml_data
 
 
 def SearchPUBMED(search_sent, recent_date = None):
 
-	POST_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=1000000'
-	moz_emu = PyMozilla.MozillaEmulator(cacher = None)
+    POST_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=1000000'
+    moz_emu = PyMozilla.MozillaEmulator(cacher = None)
 
-	search_term = search_sent.replace(' ', '+')
-	search_url = POST_URL + '&term=' + search_term
-	if recent_date:
-		search_url += '&reldate=' + str(recent_date)
+    search_term = search_sent.replace(' ', '+')
+    search_url = POST_URL + '&term=' + search_term
+    if recent_date:
+        search_url += '&reldate=' + str(recent_date)
 
-	xml_data = moz_emu.download(search_url, trycount = 3)
+    xml_data = moz_emu.download(search_url, trycount = 3)
 
-	id_list = re.findall('<Id>(\d*)</Id>', xml_data)
-	id_nums = map(lambda x: int(x), id_list)
-	return id_nums
+    id_list = re.findall('<Id>(\d*)</Id>', xml_data)
+    id_nums = map(lambda x: int(x), id_list)
+    return id_nums
