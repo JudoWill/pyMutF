f26c52da15f8df72c174826747a2093893e11432
Will Dampier <judowill@gmail.com>
2010-06-22 18:33:33 -0400

    added a set of annotation models

---
+++ DistAnnot/Annot/migrations/0001_initial.py
@@ -0,0 +1,121 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'MutationAnnot'
+        db.create_table('Annot_mutationannot', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('User', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('Mutation', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['Interaction.Mutation'])),
+        ))
+        db.send_create_signal('Annot', ['MutationAnnot'])
+
+        # Adding model 'InteractionEffectAnnot'
+        db.create_table('Annot_interactioneffectannot', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('User', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('InteractionEffect', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['Interaction.InteractionEffect'])),
+        ))
+        db.send_create_signal('Annot', ['InteractionEffectAnnot'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'MutationAnnot'
+        db.delete_table('Annot_mutationannot')
+
+        # Deleting model 'InteractionEffectAnnot'
+        db.delete_table('Annot_interactioneffectannot')
+
+
+    models = {
+        'Annot.interactioneffectannot': {
+            'InteractionEffect': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['Interaction.InteractionEffect']"}),
+            'Meta': {'object_name': 'InteractionEffectAnnot'},
+            'User': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'Annot.mutationannot': {
+            'Meta': {'object_name': 'MutationAnnot'},
+            'Mutation': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['Interaction.Mutation']"}),
+            'User': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'Interaction.gene': {
+            'Entrez': ('django.db.models.fields.IntegerField', [], {}),
+            'Meta': {'object_name': 'Gene'},
+            'Name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'Organism': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'Interaction.interaction': {
+            'HIVGene': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'HIVPartner'", 'to': "orm['Interaction.Gene']"}),
+            'HumanGene': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'HumanPartner'", 'to': "orm['Interaction.Gene']"}),
+            'InteractionType': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['Interaction.InteractionType']"}),
+            'Meta': {'object_name': 'Interaction'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'Interaction.interactioneffect': {
+            'Interaction': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['Interaction.Interaction']"}),
+            'Meta': {'object_name': 'InteractionEffect'},
+            'Mutation': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['Interaction.Mutation']"}),
+            'Type': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'Interaction.interactiontype': {
+            'Meta': {'object_name': 'InteractionType'},
+            'Type': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'Interaction.mutation': {
+            'Gene': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['Interaction.Gene']", 'null': 'True', 'blank': 'True'}),
+            'Interaction': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['Interaction.Interaction']", 'through': "orm['Interaction.InteractionEffect']", 'symmetrical': 'False'}),
+            'Meta': {'object_name': 'Mutation'},
+            'Mut': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['Annot']
--- DistAnnot/Annot/models.py
+++ DistAnnot/Annot/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from Interaction.models import Mutation, InteractionEffect
 
 # Create your models here.
+class MutationAnnot(models.Model):
+
+    User = models.ForeignKey(User)
+    Mutation = models.ForeignKey(Mutation)
+    
+
+class InteractionEffectAnnot(models.Model):
+
+    User = models.ForeignKey(User)
+    InteractionEffect = models.ForeignKey(InteractionEffect)
