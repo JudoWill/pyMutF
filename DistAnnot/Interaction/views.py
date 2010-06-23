326641dd5238d68799c985f22c673b4a5c6e2687
Will Dampier <dampier.william@gmail.com>
2010-06-23 14:13:08 -0400

    fixed the splash-page view to use the relevant context variables

--- DistAnnot/Interaction/views.py
+++ DistAnnot/Interaction/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.utils.translation import ugettext as _
 from django.conf import settings
+from django.template import RequestContext
 # Create your views here.
 
 
@@ -8,7 +9,6 @@ from django.conf import settings
 def index(request):
 
     odict = {'user': request.user}
-    return render_to_response('index.html', odict)
+    return render_to_response('index.html', 
+			context_instance = RequestContext(request))
 
-def login(request):
-    return render_to_response('index.html', odict) #obviously need to fix this
