24d751ec7b8b7bf83ecb8437680f7249bf24f2c9
Will Dampier <judowill@gmail.com>
2010-06-23 17:35:18 -0400

    added some extra fields for making things easier

--- DistAnnot/Annot/models.py
+++ DistAnnot/Annot/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-from Interaction.models import Mutation, InteractionEffect, Gene
+from Interaction.models import Mutation, InteractionEffect, Gene, EffectType
 
 # Create your models here.
 class MutationAnnot(models.Model):
 
     User = models.ForeignKey(User)
     Mutation = models.ForeignKey(Mutation)
+    GeneChosen = models.ForeignKey(Gene)
     
 
 class InteractionEffectAnnot(models.Model):
 
     User = models.ForeignKey(User)
     InteractionEffect = models.ForeignKey(InteractionEffect)
+    EffectChosen = models.ForeignKey(EffectType)
