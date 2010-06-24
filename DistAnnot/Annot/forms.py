ce29936443b9f4ea63ee4a1b8aeadc8d780af905
Will Dampier <judowill@gmail.com>
2010-06-24 19:30:45 -0400

    added validation to the AnnotForm

--- DistAnnot/Annot/forms.py
+++ DistAnnot/Annot/forms.py
@@ -20,4 +20,21 @@ class AnnotForm(forms.Form):
                                  widget = AutoCompleteTagInput(attrs = {'queryset':Gene.objects.filter(Organism = 'Human').values('Name', 'Entrez')}))
     HIV_gene = forms.CharField(required = False, max_length = 256,
                                  widget = AutoCompleteTagInput(attrs = {'queryset':Gene.objects.filter(Organism = 'HIV').values('Name', 'Entrez')}))
+    SentenceID = forms.IntegerField(widget = forms.HiddenInput)
     
+    def clean(self):
+
+        cleaned_data = self.cleaned_data
+        cleaned_data['Bad_extraction'] = cleaned_data['Bad_extraction'].strip()[:-1]
+        cleaned_data['No_mutation'] = cleaned_data['No_mutation'].strip()[:-1]        
+
+        if cleaned_data['Bad_extraction'] or cleaned_data['No_mutation']:
+            return cleaned_data
+        
+        if len(cleaned_data['Human_gene']) == 0 and len(cleaned_data['HIV_gene']) == 0:
+            raise forms.ValidationError('You must specify at least ONE protein')
+
+        if cleaned_data['Human_gene'] and cleaned_data['HIV_gene']:
+            raise forms.ValidationError('You can only specify ONE protein')
+
+        return cleaned_data
--- DistAnnot/Annot/urls.py
+++ DistAnnot/Annot/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     # (r'^DistAnnot/', include('DistAnnot.foo.urls')),
 
     url(r'LabelMutation.html', 'Annot.views.LabelMutation', name = 'LabelMutation'),
-
+    url(r'/(?P<SentID>\d*)/LabelMutation.html', 'Annot.views.LabelMutation', name = 'LabelMutation'),
 )
