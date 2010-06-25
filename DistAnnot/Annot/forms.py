1fba513b2701e7fd1c93ae3d92f10af7456d9b17
Will Dampier <judowill@gmail.com>
2010-06-25 11:47:01 -0400

    changed the form ... now a user should annotate all of the genes mentioned in the sentence and then choose the one that has the mutation

--- DistAnnot/Annot/forms.py
+++ DistAnnot/Annot/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 from django import forms
 from DistAnnot.Annot.models import MutationAnnot
 from DistAnnot.Interaction.models import Gene
-from widgets import AutoCompleteTagInput
+from widgets import AutoCompleteTagInput, AutoCompleteTagInputLarge
 
 from operator import attrgetter
 from django.db.models.query_utils import Q
@@ -40,10 +40,40 @@ class ChoiceGeneField(forms.ModelChoiceField):
         pass
 
 
-class MutationAnnotForm(ModelForm):
-    class Meta:
-        model =  MutationAnnot
-        exclude = ['User']
+class MultiChoiceGeneField(ChoiceGeneField):
+
+    def __init__(self, queryset, *args, **kwargs):
+
+        self.queryset = queryset
+        self.mapping = {'GN':attrgetter('Name'),
+                        'EN':attrgetter('Entrez')}
+        self.qresolve = {'GN': lambda x: Q(Name=x),
+                         'EN': lambda x: Q(Entrez=int(x))}
+        kwargs['widget'] = AutoCompleteTagInputLarge(attrs = {'queryset':queryset,
+                                                         'mapping':self.mapping})
+        kwargs['queryset'] = queryset
+        super(ChoiceGeneField, self).__init__(*args, **kwargs)
+
+
+    def to_python(self, value):
+
+        if not value:
+            return None
+
+        labels = map(lambda x: x.lstrip().rstrip(), value.split(','))
+        ids = []
+        for label in labels:
+            vparts = label.split(':')
+            typ = vparts[0]
+            ref = vparts[1].split(',')[0]
+
+            Qgen = self.qresolve[typ]
+            Qobj = Qgen(ref)
+            ids.append(Gene.objects.get(Qobj).id)
+
+        return Gene.objects.filter(id__in = ids).distinct()
+
+
 
 class AnnotForm(forms.Form):
 
@@ -51,8 +81,8 @@ class AnnotForm(forms.Form):
                                         help_text = 'Check this box if the text displayed is just jibberish')
     No_mutation = forms.BooleanField(required = False,
                                      help_text = 'Check this box if there are no mentions of a mutation in this text')
-    Human_gene = ChoiceGeneField(Gene.objects.filter(Organism = 'Human'), required = False)
-    HIV_gene = ChoiceGeneField(Gene.objects.filter(Organism = 'HIV'), required = False)
+    MentionedGenes = MultiChoiceGeneField(Gene.objects.all(), required = False)
+    MutatedGene = ChoiceGeneField(Gene.objects.all(), required = False)
     SentenceID = forms.IntegerField(widget = forms.HiddenInput)
     MutID = forms.IntegerField(widget = forms.HiddenInput)
     
--- DistAnnot/Annot/widgets.py
+++ DistAnnot/Annot/widgets.py
@@ -41,3 +41,41 @@ class AutoCompleteTagInput(forms.TextInput):
                 autoFill: true,
             });
             </script>''' % (name, tag_list))
+
+class AutoCompleteTagInputLarge(forms.Textarea):
+    class Media:
+        css = {
+            'all': (settings.MEDIA_URL+'/'+'jquery.autocomplete.css',)
+        }
+        js = (
+            settings.MEDIA_URL+'/'+'lib/jquery.js',
+            settings.MEDIA_URL+'/'+'lib/jquery.bgiframe.min.js',
+            settings.MEDIA_URL+'/'+'lib/jquery.ajaxQueue.js',
+            settings.MEDIA_URL+'/'+'jquery.autocomplete.js'
+        )
+
+    def render(self, name, value, attrs=None):
+        """Must be passed a QuerySet!!!"""
+        output = super(AutoCompleteTagInputLarge, self).render(name, value, attrs)
+
+        qset = self.attrs['queryset']
+        mapping = self.attrs['mapping']
+        tag_items = []
+        for item in qset:
+            for key, fun in mapping.items():
+                tag_items.append(key+':'+str(fun(item)))
+        tag_list = simplejson.dumps(tag_items,
+                                    ensure_ascii=False)
+        return output + mark_safe(u'''<script type="text/javascript">
+            jQuery("#id_%s").autocomplete(%s, {
+                width: 700,
+                max: 10,
+                highlight: false,
+                multiple: true,
+                multipleSeparator: ", ",
+                scroll: true,
+                scrollHeight: 300,
+                matchContains: true,
+                autoFill: true,
+            });
+            </script>''' % (name, tag_list))
