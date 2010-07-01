2f0faec41185a489a83078d51749cb206b57401c
Will Dampier <judowill@gmail.com>
2010-07-01 18:20:40 -0400

    added a test for downloading articles and parsing out mutations

--- DistAnnot/Annot/tests.py
+++ DistAnnot/Annot/tests.py
@@ -6,18 +6,21 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from DistAnnot.Interaction.models import *
+
+
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+    def test_article_retrieval(self):
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+        test_articles = [ {'PMID':14694110, 'PMCID':'PMC368750', 'res':'K298A'},
+                              {'PMID':10074138, 'PMCID':'PMC104048', 'res':'F16A'}]
 
->>> 1 + 1 == 2
-True
-"""}
+        for art in test_articles:
+            nart = Article(PMID = art['PMID'], PMCID = art['PMCID'])
+            nart.ReadMuts()
+            self.assertTrue(Mutation.objects.filter(Mut = art['res']).exists())
+
+
+
 
