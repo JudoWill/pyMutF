9feac829cd4ede104904c1c51e0767295e9f523e
Will Dampier <judowill@gmail.com>
2010-07-01 18:23:30 -0400

    moved Test-article to the proper location

--- DistAnnot/Annot/tests.py
+++ DistAnnot/Annot/tests.py
@@ -6,20 +6,7 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from DistAnnot.Interaction.models import *
 
-
-
-class SimpleTest(TestCase):
-    def test_article_retrieval(self):
-
-        test_articles = [ {'PMID':14694110, 'PMCID':'PMC368750', 'res':'K298A'},
-                              {'PMID':10074138, 'PMCID':'PMC104048', 'res':'F16A'}]
-
-        for art in test_articles:
-            nart = Article(PMID = art['PMID'], PMCID = art['PMCID'])
-            nart.ReadMuts()
-            self.assertTrue(Mutation.objects.filter(Mut = art['res']).exists())
 
 
 
