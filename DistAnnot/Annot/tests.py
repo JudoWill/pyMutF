e9c86e32957ab7b4fc93f073de696760310eeb33
Will Dampier <judowill@gmail.com>
2010-07-02 10:08:38 -0400

    added a few tests!

--- DistAnnot/Annot/tests.py
+++ DistAnnot/Annot/tests.py
@@ -6,8 +6,31 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from DistAnnot.Interaction.models import *
+from forms import AnnotForm, InteractionEffectForm
+from django.forms.formsets import formset_factory
+
+class SimpleTest(TestCase):
+    
+    fixtures = ['Interaction.simple_data.yaml']
+
+    def test_jibberish(self):
+        print Article.objects.all().count()
+        sent = Sentence.objects.all()[0]
+        mut = sent.Mutation.all()[0]
+
+        data = {'effect-SentenceID': sent.id,
+                'effect-MutID': mut.id,
+                'effect-BadExtraction':True}
 
 
+        resp = self.client.post(reverse('LabelMutation'), data)
+
+        self.assertFalse(Sentence.objects.filter(id = sent.id).exists())
+
+
+        
 
 
 
