325341d7f0ffea8745e08a9607b320a8e8514131
Will Dampier <judowill@gmail.com>
2010-07-13 18:03:34 -0400

    changed settings for a local database

--- DistAnnot/settings.py
+++ DistAnnot/settings.py
@@ -13,10 +13,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pyMutF',                      # Or path to database file if using sqlite3.
-        'USER': 'pyMutF',                      # Not used with sqlite3.
-        'PASSWORD': 'password',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'database.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': ''                                    # Set to empty string for default. Not used with sqlite3.
     },
