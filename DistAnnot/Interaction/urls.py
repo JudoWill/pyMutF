e9564b744f91a71c73aa8a741353453fdc85509f
Will Dampier <judowill@gmail.com>
2010-07-16 13:51:12 -0400

    added tag-list and tag-detail stuff to the urls.py

--- DistAnnot/Interaction/urls.py
+++ DistAnnot/Interaction/urls.py
@@ -15,6 +15,16 @@ mutation_info = {
 	'queryset':Mutation.objects.all(),
 }
 
+tag_list_info = {
+    'template_name':'Interaction/tag_list.html',
+    'queryset':MutationTags.objects.all(),
+}
+
+tag_info = {
+    'template_name':'Interaction/tag_detail.html',
+    'queryset':MutationTags.objects.all()
+}
+
 
 urlpatterns = patterns('',
      url(r'^stats.html', 'Interaction.views.stats', name = 'stats'),
@@ -24,4 +34,7 @@ urlpatterns = patterns('',
      url(r'^article/(?P<object_id>\d*)/detail.html', list_detail.object_detail, article_info, name = 'article_detail'),
      url(r'^mutation/(?P<object_id>\d*)/detail.html', list_detail.object_detail, mutation_info, name = 'mutation_detail'),
      url(r'^mutation/(?P<object_id>\d*)/tag.html', 'Interaction.views.TagMutation', name = 'mutation_tag'),
+     url(r'^tag/tag_list.html', list_detail.object_list, tag_list_info, name = 'tag_list'),
+     url(r'^tag/(?P<object_id>\d*)/detail.html', list_detail.object_detail, tag_info, name = 'tag_detail'),
+     
 )
