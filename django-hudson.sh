dc224692e226b892bd1b91f5c81074dde109a117
Will Dampier <judowill@gmail.com>
2010-07-23 19:39:48 -0400

    missed requirement

--- django-hudson.sh
+++ django-hudson.sh
@@ -3,8 +3,8 @@ virtualenv ve --no-site-packages
 source ./ve/bin/activate
 pip install -e git://github.com/davedash/django-fixture-magic#egg=django_fixture_magic
 pip install -e git://github.com/cmheisel/nose-xcover.git#egg=nosexcover
-pip install -f http://pyyaml.org/download/pyyaml/PyYAML-3.09.tar.gz
-pip install -f http://nltk.googlecode.com/files/nltk-2.0b8.tar.gz
+pip install PyYAML -f http://pyyaml.org/download/pyyaml/PyYAML-3.09.tar.gz
+pip install nltk -f http://nltk.googlecode.com/files/nltk-2.0b8.tar.gz
 pip install -q -E ./ve -r requirements.pip
 
 cd ./DistAnnot
