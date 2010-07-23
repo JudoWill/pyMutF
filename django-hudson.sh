979999f166de5a2ecdef388a4f19265e69020cd2
Will Dampier <judowill@gmail.com>
2010-07-23 18:49:37 -0400

    this will eventually work

--- django-hudson.sh
+++ django-hudson.sh
@@ -1,5 +1,5 @@
-virtualenv -q ve --no-site-packages
-source ve/bin/activate
+virtualenv ve --no-site-packages
+source $WORKSPACE/ve/bin/activate
 pip install -e git://github.com/davedash/django-fixture-magic#egg=django_fixture_magic
 pip install -e git://github.com/cmheisel/nose-xcover.git#egg=nosexcover
 pip install -q -E ./ve -r requirements.pip
