75d84e7a1c8559ab8e001b7454c6747d398ccd99
Will Dampier <judowill@gmail.com>
2010-07-23 18:43:16 -0400

    this will eventually work

--- django-hudson.sh
+++ django-hudson.sh
@@ -1,7 +1,7 @@
 virtualenv -q ve --no-site-packages
-source ./ve/bin/activate
-pip install -e git://github.com/davedash/django-fixture-magic#egg=django_fixture_magic --install-dir ./ve/lib/python2.6/site-packages/
-pip install -e git://github.com/cmheisel/nose-xcover.git#egg=nosexcover --install-dir ./ve/lib/python2.6/site-packages/
-pip install -q -E ./ve -r requirements.pip --install-dir ./ve/lib/python2.6/site-packages/
+source ve/bin/activate
+pip install -e git://github.com/davedash/django-fixture-magic#egg=django_fixture_magic
+pip install -e git://github.com/cmheisel/nose-xcover.git#egg=nosexcover
+pip install -q -E ./ve -r requirements.pip
 cd ./DistAnnot
 python manage.py test --with-coverage --cover-package=DistAnnot --with-xunit --with-xcoverage Interaction
