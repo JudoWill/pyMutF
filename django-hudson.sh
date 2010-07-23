4b102060aff3e5db4d0b88c467075e0a44b3d31c
Will Dampier <judowill@gmail.com>
2010-07-23 19:35:37 -0400

    missed requirement

--- django-hudson.sh
+++ django-hudson.sh
@@ -4,5 +4,6 @@ source ./ve/bin/activate
 pip install -e git://github.com/davedash/django-fixture-magic#egg=django_fixture_magic
 pip install -e git://github.com/cmheisel/nose-xcover.git#egg=nosexcover
 pip install -q -E ./ve -r requirements.pip
+pip install -f http://nltk.googlecode.com/files/nltk-2.0b8.tar.gz
 cd ./DistAnnot
 python manage.py test --with-coverage --cover-package=DistAnnot --with-xunit --with-xcoverage Interaction
