dac725a2343756c63ec2dd06b87c38dcb5d7f4f6
Will Dampier <judowill@gmail.com>
2010-07-23 16:46:20 -0400

    fixed the test-script

--- django-hudson.sh
+++ django-hudson.sh
@@ -1,5 +1,5 @@
-cd $WORKSPACE
+cd $WORKSPACE/DistAnnot
 virtualenv -q ve
 source ./ve/bin/activate
 pip install -q -E ./ve -r requirements.pip
-django-admin.py test --settings=DistAnnot.settings --with-coverage --cover-package=DistAnnot --with-xunit --with-xcoverage
+python manage.py test --with-coverage --cover-package=DistAnnot --with-xunit --with-xcoverage Interaction
