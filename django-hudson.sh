3bd246523fb5752ec673c6c900b91f0c4bc0e183
Will Dampier <judowill@gmail.com>
2010-07-23 18:19:18 -0400

    another fix

--- django-hudson.sh
+++ django-hudson.sh
@@ -1,7 +1,7 @@
-cd $WORKSPACE/pyMutF/DistAnnot
+cd ./DistAnnot
 virtualenv -q ve
 source ./ve/bin/activate
-pip install -e git+git://github.com/davedash/django-fixture-magic#egg=django_fixture_magic
-pip install -e git+git://github.com/cmheisel/nose-xcover.git#egg=nosexcover
+pip install -e git://github.com/davedash/django-fixture-magic#egg=django_fixture_magic
+pip install -e git://github.com/cmheisel/nose-xcover.git#egg=nosexcover
 pip install -q -E ./ve -r requirements.pip
 python manage.py test --with-coverage --cover-package=DistAnnot --with-xunit --with-xcoverage Interaction
