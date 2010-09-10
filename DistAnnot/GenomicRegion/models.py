8a7537bd55ba2bd040e73bdfd27415f968fc19f7
Will Dampier <judowill@gmail.com>
2010-09-10 17:00:44 -0400

    added a more robust set of models for dealing with genes and proteins

---
+++ DistAnnot/GenomicRegion/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+
+# Create your models here.
+
+
+#Abstract Models
+
+class NameMixin(models.Model):
+    Names = models.ManyToManyField('Name')
+
+    class Meta:
+        abstract = True
+        
+class ProductMixin(models.Model):
+    pass
+    
+    class Meta:
+        abstract = True
+
+class LocationMixin(models.Model):
+    Start = models.IntegerField()
+    Stop = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+#Name Models
+
+class Name(models.Model):
+    Name = models.CharField(max_length = 255)
+    NameType = models.ForeignKey('NameType')
+
+class NameType(models.Model):
+    Type = models.CharField(max_length = 255)
+
+#Organism Related models
+
+class Organism(NameMixin):
+    pass
+
+class Genome(NameMixin):
+    Organism = models.ForeignKey(Organism)
+
+#Gene/Product related Models
+
+class Gene(NameMixin, ProductMixin):
+    Genome = models.ManyToManyField(Genome, through = 'GeneLocation')
+
+class Product(NameMixin):
+    Gene = models.ManyToManyField(Gene, through = 'ProductLocation')
+
+#Location related models
+
+class GeneLocation(LocationMixin):
+    Gene = models.ForeignKey('Gene')
+    Genome = models.ForeignKey(Genome)
+
+class ProductLocation(models.Model):
+    Product = models.ForeignKey('Product')
+    Gene = models.ForeignKey('Gene')
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
