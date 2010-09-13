f687c6b6a1530bbad6e2ae8e29c7fa1e46af567b
Will Dampier <judowill@gmail.com>
2010-09-13 15:02:29 -0400

    added some tests ... still need to write the templates though

--- DistAnnot/GenomicRegion/tests.py
+++ DistAnnot/GenomicRegion/tests.py
@@ -6,18 +6,29 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from views import *
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class GenomeViewsTest(TestCase):
+    fixtures = ['smallhiv']
+    view_class = GenomeViews
+    model = Genome
+    base_id = 'refseq'
+    msg_prefix = 'genome'
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+    def test_list_by_slug(self):
+        resp = self.client.get(reverse(self.view_class.list_by_slug,
+                                       kwargs = {'slug':'refseq'}))
+        for object in self.model.objects.all():
+            for name in object.Names.all():
+                self.assertContains(resp, name.Name,
+                                    msg_prefix = self.msg_prefix)
 
->>> 1 + 1 == 2
-True
-"""}
+    def test_detail(self):
+
+        for object in self.model.objects.all():
+            resp = self.client.get(object.get_absolute_url())
+            for name in object.Names.all():
+                self.assertContains(resp, name.Name,
+                                    msg_prefix = self.msg_prefix)
+
 
