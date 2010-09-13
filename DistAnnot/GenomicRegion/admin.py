ba2cb21d01a5613571b82e0ccbd63b703b49a584
Will Dampier <judowill@gmail.com>
2010-09-13 13:40:51 -0400

    added some admin models for debugging

--- DistAnnot/GenomicRegion/admin.py
+++ DistAnnot/GenomicRegion/admin.py
@@ -13,3 +13,18 @@ class GenomeAdmin(admin.ModelAdmin):
 admin.site.register(NameType, NameTypeAdmin)
 admin.site.register(Organism, OrganismAdmin)
 admin.site.register(Genome, GenomeAdmin)
+
+#only for debugging purposes!!!
+
+class GeneAdmin(admin.ModelAdmin):
+    pass
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+class NameAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Gene, GeneAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Name, NameAdmin)
