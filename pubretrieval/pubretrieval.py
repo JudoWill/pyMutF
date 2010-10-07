7a4235d32dbd8c4a99fa977d74f1a540901ed241
Will Dampier <judowill@gmail.com>
2010-10-07 15:17:19 -0400

    made some changes

--- pubretrieval/pubretrieval.py
+++ pubretrieval/pubretrieval.py
@@ -3,6 +3,7 @@ import urllib2
 import re, os.path, os
 import logging
 from types import FunctionType, StringType
+from random import shuffle
 #from optparse import OptionParser
 
 
@@ -39,7 +40,7 @@ def get_pdf(url, uid, path = '/results'):
         url = url.pop()
 
     logging.warning('Opening path for %s' % uid)
-    data = urllib2.open(url)
+    data = urllib2.urlopen(url)
     with open(os.path.join(path, '%s.pdf' % uid), 'w') as handle:
         logging.warning('Writting data for %s' % uid)
         handle.write(data.read())
@@ -49,7 +50,26 @@ def get_pdf(url, uid, path = '/results'):
 def get_pdf_link(sel):
     url_reg = r'http\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?.pdf'
 
-    return re.findall(url_reg, sel.get_html_source())
+    res = re.findall(url_reg, sel.get_html_source())
+    if len(res) == 0:
+
+        url_reg = r'(/\S*)?.pdf'
+        res = re.findall(url_reg, sel.get_html_source())
+        if len(res):
+            url = sel.get_location().split('/')
+            base = '/'.join(url[:3])
+            final = []
+            for r in res:
+                if r.startswith('/'):
+                    final.append(base+r)
+                else:
+                    final.append(base+'/'+r)
+            return final
+    else:
+        return res
+
+
+
 
 def process_sd(sel):
     return False
@@ -57,17 +77,29 @@ def process_sd(sel):
 def acs_custom(sel):
     return False
 
+def change_focus(sel, new_window = 'undefined'):
+    sel.select_window(new_window)
+    return True
+
+def close_window(sel):
+    sel.close()
 
 if __name__ == '__main__':
 
     results_dir = '/results/'
 
-    with open('finished.txt') as handle:
-        finished = set([x for x in handle if x.strip()])
+    with open('no_links.txt') as handle:
+        finished = set([x.strip() for x in handle if x.strip()])
+
+    with open('skipping.txt') as handle:
+        finished |= set([x.strip() for x in handle if x.strip()])
+
+    with open('missing_path.txt') as handle:
+        finished |= set([x.split(',')[0] for x in handle if x.strip()])
 
     with open('pubmed_result.txt') as handle:
         pmids = [x.strip() for x in handle if x.strip() and x not in finished]
-
+    shuffle(pmids)
 
     #this dict contains the "click" items that will bring the selenium item to a
     #page with the pdf-url
@@ -80,33 +112,33 @@ if __name__ == '__main__':
         'linkout-icon-unknown-aac_final':("link=Full Text (PDF)",),
         'linkout-icon-unknown-vir_full':("link=Full Text (PDF)",),
         'linkout-icon-unknown-acspubs':(acs_custom,),
-        'linkout-icon-unknown-oxfordjournals':None,
-        'linkout-icon-unknown-ben_pubmed':None,
-        'linkout-icon-unknown-pnas_full':None,
-        'linkout-icon-unknown-jvi_full':None,
-        'linkout-icon-unknown-anakarder':None,
+        'linkout-icon-unknown-oxfordjournals':("link=Full Text (PDF)",),
+        'linkout-icon-unknown-ben_pubmed':False,
+        'linkout-icon-unknown-pnas_full':tuple(),
+        'linkout-icon-unknown-jvi_full':tuple(),
+        'linkout-icon-unknown-anakarder':False,
         'linkout-icon-unknown-pmc':None,
-        'linkout-icon-unknown-lo_npg':None,
-        'linkout-icon-unknown-wiley_interscience_pubmed_logo_120x27':None,
-        'linkout-icon-unknown-bj_pubmed_latest':None,
-        'linkout-icon-unknown-adis1':None,
-        'linkout-icon-unknown-springerlink':None,
-        'linkout-icon-unknown-jbc_final':None,
-        'linkout-icon-unknown-jcm_final':None,
+        'linkout-icon-unknown-lo_npg':tuple(),
+        'linkout-icon-unknown-wiley_interscience_pubmed_logo_120x27':None, #
+        'linkout-icon-unknown-bj_pubmed_latest':tuple(),
+        'linkout-icon-unknown-adis1':None, #need redirect
+        'linkout-icon-unknown-springerlink':tuple(), #
+        'linkout-icon-unknown-jbc_final':("link=Full Text (PDF)",),
+        'linkout-icon-unknown-jcm_final':("link=Full Text (PDF)",),
         'linkout-icon-unknown-linkout':None,
         'linkout-icon-unknown-Button_120x27px_FullText':None,
-        'linkout-icon-unknown-aac_full':None,
-        'linkout-icon-unknown-pmlo_ijmm':None,
+        'linkout-icon-unknown-aac_full':tuple(),
+        'linkout-icon-unknown-pmlo_ijmm':False,
         'linkout-icon-unknown-MAL_100x25':None,
-        'linkout-icon-unknown-lo_nature':None,
-        'linkout-icon-unknown-mc':None,
-        'linkout-icon-unknown-dmd_final':None,
-        'linkout-icon-unknown-annintmed_final':None,
-        'linkout-icon-unknown-jcm_full':None,
-        'linkout-icon-unknown-wjg':None,
-        'linkout-icon-unknown-MS':None,
-        'linkout-icon-unknown-bmc':None,
-        'linkout-icon-unknown-pmlogo':("//div[@id='site-left']/a/img", "//a[@id='pdf']/span"),
+        'linkout-icon-unknown-lo_nature':tuple(),
+        'linkout-icon-unknown-mc':False,
+        'linkout-icon-unknown-dmd_final':False,
+        'linkout-icon-unknown-annintmed_final':("link=Full Text (PDF)",),
+        'linkout-icon-unknown-jcm_full':tuple(),
+        'linkout-icon-unknown-wjg':False,
+        'linkout-icon-unknown-MS':tuple(),
+        'linkout-icon-unknown-bmc':tuple(),
+        'linkout-icon-unknown-pmlogo':False
         #'linkout-icon-unknown-PubMedLink':(process_sd, ),
         #'linkout-icon-unknown-cellhub':(process_sd),
     }
@@ -114,8 +146,14 @@ if __name__ == '__main__':
 
     sel = selenium("localhost", 4444, "*chrome", "http://www.ncbi.nlm.nih.gov/")
     sel.start()
+    sel.open('about:blank')
+    raw_input()
 
-    out_handle = open('finished.txt', 'a')
+
+    no_links = open('no_links.txt', 'a')
+    missing_path = open('missing_path.txt', 'a')
+    skipping = open('skipping.txt', 'a')
+
 
     for pmid in pmids:
         found = do_search(sel, pmid)
@@ -126,10 +164,17 @@ if __name__ == '__main__':
                     path = path_dict[linkout]
                 except KeyError:
                     logging.warning('No path for linkout!: %s at pmid:%s' % (linkout, pmid))
+                    missing_path.write('%s,%s\n' % (pmid, linkout))
                     continue
-                if path is not None:
+                if path:
                     sel.click(linkout)
-                    sel.wait_for_page_to_load("30000")
+                    sel.select_pop_up('null')
+                    print sel.get_all_window_titles()
+                    try:
+                        sel.wait_for_page_to_load("30000")
+                    except:
+                        pass
+                    print sel.get_all_window_titles()
                     further_dl = True
                     for link in path:
 
@@ -137,16 +182,20 @@ if __name__ == '__main__':
                             print 'clicking: ', link
                             sel.click(link)
                             sel.wait_for_page_to_load("30000")
+
                         elif type(link) == FunctionType:
                             further_dl = link(sel)
                     if further_dl:
                         pdf_url = get_pdf_link(sel)
                         if pdf_url:
                             get_pdf(pdf_url, pmid, path = results_dir)
-                else:
-                    continue
-            out_handle.write(pmid+'\n')
+                elif path is False:
+                    skipping.write(pmid+'\n')
+                elif path is None:
+                    missing_path.write('%s,%s\n' % (pmid, linkout))
 
+        else:
+            no_links.write(pmid+'\n')
 
 
 
